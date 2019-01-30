30a3005bb56323ebe3f70e01c215d19824048bc9
Mauro <mauro.granata.99@gmail.com>
2019-01-30 18:49:57 +0100

    Added material-ui dependencie

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.157e54cb21c741443bccc2b1925be19b.js"
+  "/Hasse-Diagram/build/precache-manifest.992ed4f5d47e1c00a3b22e423fa33e3c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Hasse-Diagram/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
